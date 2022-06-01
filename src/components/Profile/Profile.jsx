62c4881e3f7cf73c1d67edfa66b18626b152c225
Nazariy Bakush <nazkamaz@gmail.com>
2022-06-01 13:42:57 +0300

    hw1 task1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+import styles from 'components/Profile/Profile.module.css';
+
+// console.log(user)
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className={styles.profile}>
+      <div className="description">
+        <img
+          src={avatar}
+          alt={username}
+          className="avatar"
+        />
+        <h2 className="name">{username}</h2>
+        <p className={styles.tag}>{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        <li className={styles.statsItem}>
+          <span className="label">Followers</span>
+          <span className="quantity">  {stats.followers}</span>
+        </li>
+        <li className={styles.statsItem}>
+          <span className="label">Views</span>
+          <span className="quantity">  {stats.views}</span>
+        </li>
+        <li className={styles.statsItem}>
+          <span className="label">Likes</span>
+          <span className="quantity">  {stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+username: PropTypes.string.isRequired,
+tag: PropTypes.string.isRequired,
+location: PropTypes.string.isRequired,
+avatar: PropTypes.node.isRequired,
+stats: PropTypes.object.isRequired,
+followers: PropTypes.number.isRequired,
+views: PropTypes.number.isRequired,
+likes: PropTypes.number.isRequired,
+};
+
+
+export default Profile
+
